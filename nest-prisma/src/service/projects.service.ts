bccfad9eb74b00daa426ef75936ebb1edfcaaa70
MateusGCN <mateus.neves@sou.inteli.edu.br>
2023-03-07 15:22:42 -0300

    fix(projects.service): fixed creator

--- nest-prisma/src/service/projects.service.ts
+++ nest-prisma/src/service/projects.service.ts
@@ -6,7 +6,7 @@ import { ProjectsRepository } from 'src/modules/projects/projects.repository';
 export class ProjectsService {
     constructor(private repository: ProjectsRepository) { }
 
-    async createProject(params: { idProject: Project[`idProject`], title: Project[`title`]; description: Project[`description`], submission_date: Project[`submission_date`], period: Project[`period`], specification: Project[`specification`], area: Project[`area`], role: Project[`role`], auth: Project[`auth`], creator: Profile[`email`] }) {
+    async createProject(params: { idProject: Project[`idProject`], title: Project[`title`]; description: Project[`description`], submission_date: Project[`submission_date`], period: Project[`period`], specification: Project[`specification`], area: Project[`area`], role: Project[`role`], auth: Project[`auth`], creator: Project[`creator`] }) {
         const { idProject, title, description, submission_date, period, specification, area, role, auth, creator } = params;
 
         const project = await this.repository.createProject({
