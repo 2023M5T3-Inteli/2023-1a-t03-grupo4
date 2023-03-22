76d458d952d3729ddfdd1418bd2431104d545701
mikemouadeb <mike.mouadeb@sou.inteli.edu.br>
2023-03-22 16:55:34 -0300

    Footer feito

--- src/frontend/src/components/Footer.jsx
+++ src/frontend/src/components/Footer.jsx
@@ -1,8 +1,34 @@
-function Footer ()
-{
-    return (<div calssName="w-full h-26 z-50 fle" style={{backgroundColor:"var(--grey1)", width:"100%", height:"10rem"}}>
-        <h1>Entre em contato Sobre os desenvolvedores Testetetstetstetes</h1>
-    </div>)
+import React from 'react';
+import Dell from '../assets/svg/DellLogo/export'
+import CNL from '../assets/images/CNLogoName.png'
+import './components_css/Footer.css'; 
+
+function Footer() {
+  return (
+    <div className="footer-container">
+    
+    <div className="footer-img1">
+        <Dell width={"6rem"}/>
+    </div>
+
+    <div className="footer-img2">
+        <img src={CNL} alt="" />
+    
+    </div>
+
+    <div className="flex-col">
+      <p className="footer-text2">Entre em Contato</p>
+      <p className="footer-text2">Sobre os desenvolvedores</p>
+    </div> 
+
+    <div className="flex-col">
+      <p className="footer-text">Entre em Contato</p>
+      <p className="footer-text">Políticas de segurança</p>
+      <p className="footer-text">FAC</p>
+    </div> 
+
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
