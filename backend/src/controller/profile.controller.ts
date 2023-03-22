7f10a34c42e4617e4aaa00a6131a664850751412
MateusGCN <mateus.neves@sou.inteli.edu.br>
2023-03-22 11:53:46 -0300

    add profile, submission controllers

---
+++ backend/src/controller/profile.controller.ts
@@ -0,0 +1,34 @@
+import { Body, Controller, Delete, Get, Param, Post, Put, } from '@nestjs/common';
+import createProfileDto from '../module/dto/createProfile.dto';
+import { profileService } from '../services/profile.service'; 
+
+
+@Controller('profile')
+export class profileController {
+  constructor(private readonly profileService: profileService) {}
+
+  // get all todos
+  @Get()
+  getProfile() {
+    return this.profileService.getAllProfiles();
+  }
+
+  // get todo by id
+  @Get(':id')
+  getProfileById(@Param('id') id: number) {
+    return this.profileService.getProfileById(Number(id));
+  }
+
+  // create todo
+  @Post()
+  async createProfile(@Body() profile: createProfileDto) {
+    return this.profileService.createProfile(profile);
+  }
+
+
+  //delete todo
+  @Delete(':id')
+  async deleteprofile(@Param('id') id: string) {
+    this.profileService.deleteprofile(Number(id));
+  }
+}
