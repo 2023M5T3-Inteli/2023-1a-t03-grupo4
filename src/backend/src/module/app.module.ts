0ed97dbd27bb9c6e763060f7f886b19c27589157
Beatriz Hirasaki <beatriz.leite@sou.inteli.edu.br>
2023-03-31 16:21:17 -0300

    Host e password mudados

--- src/backend/src/module/app.module.ts
+++ src/backend/src/module/app.module.ts
@@ -24,10 +24,10 @@ import { HealthModule } from './health.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'grupo4-dev.cd53msdn47fe.us-east-1.rds.amazonaws.com',
+      host: database-bia-prova.ckt8kbdripg4.us-east-1.rds.amazonaws.com
       port: 5432,
       username: 'postgres',
-      password: 'grupo4123',
+      password: 'senhabiaprova',
       database: 'postgres',
       entities: [Project, Profile, Skills, Positions, Submission, Technologies, Login],
       synchronize: true,
