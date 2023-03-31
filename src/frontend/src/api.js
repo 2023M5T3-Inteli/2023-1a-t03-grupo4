cc9c70f1f1f45ce7829a2b4c6db9b2fc12ba7e0c
Priscila Falcão <priscila.santos@sou.inteli.edu.br>
2023-03-31 17:05:38 -0300

    Prova

--- src/frontend/src/api.js
+++ src/frontend/src/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'http:prova-lb-873497458.us-east-1.elb.amazonaws.com',
+    baseURL: 'http://prova-lb-873497458.us-east-1.elb.amazonaws.com',
 });
