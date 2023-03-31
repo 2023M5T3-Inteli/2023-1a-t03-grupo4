6ad9691d2a2a230fc576077ef5cdc2dad417e2ee
WagnerBarcelos <wagner.silva@sou.inteli.edu.br>
2023-03-31 15:40:10 -0300

    Mudando variáveis do backend

--- src/backend/src/module/app.module.ts
+++ src/backend/src/module/app.module.ts
@@ -24,10 +24,10 @@ import { HealthModule } from './health.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'grupo4-dev.cd53msdn47fe.us-east-1.rds.amazonaws.com',
+      host: 'wags-db.clpd8ad5a9ib.us-east-1.rds.amazonaws.com',
       port: 5432,
       username: 'postgres',
-      password: 'grupo4123',
+      password: 'senhawagner',
       database: 'postgres',
       entities: [Project, Profile, Skills, Positions, Submission, Technologies, Login],
       synchronize: true,
